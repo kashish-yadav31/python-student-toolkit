1f56d0e6d618c12260055f859ab9a65e0c306591
kashish <heyiamkashishyadav@gmail.com>
2025-12-16 18:52:31 +0530

    Day 1: Added student info input

---
+++ day1_basics/student_info.py
@@ -0,0 +1,9 @@
+print("Welcome to student Toolkit")
+
+name=input("Enter student name:")
+roll_no=input("Enter roll number:")
+
+print("\nStudent Deatails")
+print("Name:",name)
+print("Roll no:",roll_no)
+
