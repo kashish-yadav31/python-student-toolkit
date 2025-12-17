cd9f8907847ae30894f997b9bca640b5a4d74c71
kashish <heyiamkashishyadav@gmail.com>
2025-12-17 18:10:38 +0530

    Day 2: Added marks and percentage calculation

--- day1_basics/student_info.py
+++ day1_basics/student_info.py
@@ -7,3 +7,15 @@ print("\nStudent Deatails")
 print("Name:",name)
 print("Roll no:",roll_no)
 
+print("\nEnter Marks")
+
+maths=int(input("Enter Maths marks:"))
+science=int(input("Enter Science marks:"))
+english=int(input("Enter English marks:"))
+
+total = maths + science + english
+percentage = total / 3
+
+print("\nResult")
+print("Total Marks:",total)
+print("Percentage:",percentage)
